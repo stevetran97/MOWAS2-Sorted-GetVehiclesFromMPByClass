5c8564934babd92dd31718ab150c79e6df721c8b
stevetran97 <65732396+stevetran97@users.noreply.github.com>
2021-03-20 21:59:17 -0400

    Clarity: Comments made more descriptive and concise | Variable name changes to match purpose | Trimming: removed unused code from sortClass | writeOutput formatting: new formats into the inside of a 2 level hash table

--- extract.py
+++ extract.py
@@ -6,7 +6,7 @@ import os
 # ----------------------------------------------------------------------------
 # Input Variables
 
-# Replacement pattern order must match the order of replacement text below
+# Vehicle name pattern to identify vehicle unit on line
 # Include all english characters and english special characters (without space)
 vehicleNamePattern = r'\"[a-zA-z0-9~@#$^*()_+=[\]|\\,.?:-]+\"'
 print("vehicleNamePattern = ", vehicleNamePattern)
@@ -21,41 +21,40 @@ commentTrigger = ';'
 # Variables
 # Sorting classes
 sortClasses = {
-    'lightmgcar': ["c(15)", "cp(3)", "b(v1)"],
-    'heavymgcar': ["c(15)", "cp(5)", "b(v1)"],
-    'transport': ["c(5)", "cp(2)", "b(v1)"],
-    'logi': ["c(15)", "cp(2)", "b(v1)"],
-    'emplacements': ["c(5)", "cp(5)", "b(v2)"],
-    'hmg': ["c(5)", "cp(5)", "b(v2)"],
-    'minigun': ["c(5)", "cp(5)", "b(v2)"],
-    'gl': ["c(5)", "cp(5)", "b(v2)"],
-    'field_artillery': ["c(5)", "cp(5)", "b(v2)"],
-    'mortar': ["c(5)", "cp(5)", "b(v2)"],
-    'aa_gun': ["c(5)", "cp(5)", "b(v2)"],
-    'atgm': ["c(5)", "cp(5)", "b(v2)"],
-    'at_gun': ["c(5)", "cp(5)", "b(v2)"],
-    'light_mortar_spg': ["c(15)", "cp(10)"],
-    'lightspgarty': ["c(20)", "cp(15)", "b(v2)"],
-    'heavyspgarty': ["c(30)", "cp(20)", "b(v2)"],
-    'lightrocketarty': ["c(30)", "cp(15)", "b(v2)"],
-    'heavyrocketarty': ["c(30)", "cp(20)", "b(v2)"],
-    'light_apc': ["c(5)", "cp(0)", "b(v3)"],
-    'medium_wheeled_apc': ["c(10)", "cp(5)", "b(v3)"],
-    'heavy_apc': ["c(20)", "cp(10)", "b(v3)"],
-    'spg_aa': ["c(15)", "cp(10)", "b(v3)"],
-    'medium_tank': ["c(15)", "cp(15)", "b(v4)"],
-    'cold_war_tank': ["c(25)", "cp(15)", "b(v4)"],
-    'heavy_cold_war_tank': ["c(30)", "cp(20)", "b(v4)"],
-    'heavy_modern_war_tank': ["c(35)", "cp(25)", "b(v4)"],
-    'very_heavy_modern_war_tank': ["c(40)", "cp(30)", "b(v4)"],
-    'tank_destroyer': ["c(15)", "cp(10)", "b(v5)"],
-    'aircraft': ["c(60)", "cp(20)", "b(special)"],
-    'light_robot_mech': ["c(40)", "cp(10)", "b(v4)"],
+    'lightmgcar',
+    'heavymgcar',
+    'transport',
+    'logi',
+    'emplacements',
+    'hmg',
+    'minigun',
+    'gl',
+    'field_artillery',
+    'mortar',
+    'aa_gun',
+    'atgm',
+    'at_gun',
+    'light_mortar_spg',
+    'lightspgarty',
+    'heavyspgarty',
+    'lightrocketarty',
+    'heavyrocketarty',
+    'light_apc',
+    'medium_wheeled_apc',
+    'heavy_apc',
+    'spg_aa',
+    'medium_tank',
+    'cold_war_tank',
+    'heavy_cold_war_tank',
+    'heavy_modern_war_tank',
+    'very_heavy_modern_war_tank',
+    'tank_destroyer',
+    'aircraft',
+    'light_robot_mech'
 }
 
-# List of vehicles default
+# Instantiate List of vehicles
 vehicleCostHashByUnit = {
-
 }
 # isCollecting trigger for enable line to be added to hash table. False by default
 isCollecting = False
@@ -69,10 +68,9 @@ fileToSearchDirectory = r'.\FilesToSearch'
 def getFilePath(fileDirectory, fileName):
     return r'{filePath}\{fileName}'.format(filePath=fileDirectory, fileName=fileName)
 
+
 # Find vehicleNamePattern in currentLine and return it
-
-
-def foundPatternInLine(text_to_search):
+def findPatternInLine(text_to_search):
     # Find pattern in the current line
     stringPatternMatch = re.findall(text_to_search, line)
     # If pattern matches, return the matched name otherwise return nothing
@@ -81,9 +79,8 @@ def foundPatternInLine(text_to_search):
     else:
         return None
 
+
 # Find Check to see if stringPatternMatch pattern is already in Hash table and add if it is not
-
-
 def checkUpdateHash(stringPatternMatch, costValue):
     # Debugging
     print("Checking stringPatternMatch = ", stringPatternMatch, "\n")
@@ -94,6 +91,7 @@ def checkUpdateHash(stringPatternMatch, costValue):
     else:
         # Adds vehicle entry to hash table if not in table
         vehicleCostHashByUnit[sortClass][stringPatternMatch] = costValue
+        print('Added Vehicle to Table: {}'.format(stringPatternMatch))
 
 
 # Formatted write resulting hashtable to output
@@ -107,20 +105,25 @@ def writeOutput(outputText):
 
     # Write from each sortClass
     for sortClass in vehicleCostHashByUnit:
+        # Write python comment denoting vehicle Class
         d.write('\n#{}\n'.format(sortClass))
+        # Write first half of vehicle Class hash table format
+        d.write('"%s" : {\n' % (sortClass))
+
         # Write from subhash
         for entry in vehicleCostHashByUnit[sortClass]:
-            # Format handling
+            # Format handling (Currently Redundant due to txt file preformatting)
             if type(vehicleCostHashByUnit[sortClass][entry]) == list:
                 rawCost = vehicleCostHashByUnit[sortClass][entry][0]
                 rawCost = re.findall(r'[0-9]+', rawCost)[0]
             else:
                 rawCost = vehicleCostHashByUnit[sortClass][entry]
-            d.write('{}: {},\n'.format(entry, rawCost))
+            d.write('\t{}: {},\n'.format(entry, rawCost))
+        # Write second half of vehicle Class hash table format
+        d.write('},')
+
 
 # Sorts any hashTable alphabetically and returns sorted hash table
-
-
 def sortHashAlphab(hashTable):
     sortedKeys = sorted(hashTable)
     newSortedHash = {}
@@ -131,11 +134,11 @@ def sortHashAlphab(hashTable):
 
 # ----------------------------------------------------------------------------
 # Main Code/ Algorithm
+# Loops through all files multiple times (One sort class for one round)
 for sortClass in sortClasses:
-
+    # Initialize vehicle class subhash
     vehicleCostHashByUnit[sortClass] = {}
-    print('\n\nInitialize subhash in {} = '.format(sortClass),
-          vehicleCostHashByUnit[sortClass])
+    # Loop through files in edit directory
     for fileName in os.listdir(fileToSearchDirectory):
         # Get filePath for fileinput
         filePath = getFilePath(fileToSearchDirectory, fileName)
@@ -143,55 +146,34 @@ for sortClass in sortClasses:
             for line in file:
                 # If line starts with comment Trigger
                 if line[0] == commentTrigger:
-                    # If runs into the sortClass on that line (Must start with comment trigger)
+                    # If loop runs into the sort Class on that line (Must start/end with comment trigger)
                     if re.findall(';{};'.format(sortClass), line):
                         # Activate collecting mode to search lines
                         isCollecting = True
                         # continue off of the comment line to prevent search of comment line
                         continue
                     else:
-                        # Turns off collection mode when it runs into another comment
+                        # Turns off collection mode when it runs into another comment without class
                         isCollecting = False
 
                 # isCollecting must be true AND line must not start with comment trigger to search and possibly list in hash
                 if isCollecting == True and line[0] != commentTrigger:
-                    # Get vehicle name from searching line helper
-                    stringPatternMatch = foundPatternInLine(vehicleNamePattern)
+                    # Get vehicle name (in array) from pattern finding helper
+                    stringPatternMatch = findPatternInLine(vehicleNamePattern)
 
                     # If vehicle name is found in that line exists
                     if stringPatternMatch:
-                        # Get costValue
-                        # Cost will always accompany string match
+                        # Get formatted cost parameter: Cost will always accompany string match in this case
                         costPatternMatch = re.findall(costPattern, line)
-                        # format to integer and remove string parts
+                        # Extract/ Format to integer and remove string parts
                         formattedCost = int(re.findall(
                             r'[0-9]+', costPatternMatch[0])[0])
 
-                        # Check and maybe Update Hash table with info
+                        # Check then Update Hash table with new entry
                         checkUpdateHash(
                             stringPatternMatch[0], formattedCost)
-            # Reset isCollecting mode to default at end of file
+            # Reset isCollecting mode to default at end of file (Redundancy)
             isCollecting = False
 
 # Print final resulting table
 writeOutput(vehicleCostHashByUnit)
-
-
-# print('\n\nInitialize mainhash in vehicleCostHashByUnit = ',
-#       vehicleCostHashByUnit)
-# print("Type: ", type(stringPatternMatch), "\n\n")
-# print("vehicleCostHashByUnit", vehicleCostHashByUnit, "\n")
-# print("Type: ", type(vehicleCostHashByUnit), "\n\n")
-# print('\n\ncurrent sortClass = ', sortClass)
-# print("EndResult = ", vehicleCostHashByUnit)
-# print('\n\nFinal mainhash in = ',
-#       vehicleCostHashByUnit)
-
-# print('\n\nFinal subhash in {} = '.format(sortClass),
-#         vehicleCostHashByUnit[sortClass])
-
-# print("Unsorted lightmgcar Hash = ",
-#       vehicleCostHashByUnit["lightmgcar"])
-# print("Sorted lightmgcar Hash = ", sorted(
-#     vehicleCostHashByUnit["lightmgcar"]))
-# print("sortHashAlphab Function = ", )
